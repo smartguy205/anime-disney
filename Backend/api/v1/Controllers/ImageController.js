cc94f3232f076e23460aa5d3a6c95acd36abd6f0
deep-explorer <125335170+smartguy205@users.noreply.github.com>
2023-03-16 11:47:39 -0400

    add charactera and list

--- Backend/api/v1/Controllers/ImageController.js
+++ Backend/api/v1/Controllers/ImageController.js
@@ -1,10 +1,8 @@
 const tryCatchAsync = require("../../../util/tryCatchAsync");
 const apiResponse = require("../../../util/apiResponse");
 
-
 exports.imageUpload = tryCatchAsync(async (req, res, next) => {
-
-	const url = req.file.path;
-	let response_data = { url };
-	return apiResponse.successResponse(res, response_data, "success", 200);
+  const url = req.file.path;
+  let response_data = { url };
+  return apiResponse.successResponse(res, response_data, "success", 200);
 });
--- Frontend/src/pages/SailorMoon/SailorMoon.tsx
+++ Frontend/src/pages/SailorMoon/SailorMoon.tsx
@@ -4,6 +4,14 @@ import SailorA from "assets/Sailor Moon 1.png";
 import SailorB from "assets/Sailor Moon 2.png";
 import SailorC from "assets/Sailor Moon 3.png";
 import SailorD from "assets/Sailor Moon 4.png";
+import Quinn1 from "assets/quinn.png";
+import Quinn2 from "assets/quinn1.png";
+import Quinn3 from "assets/quinn2.png";
+import Quinn4 from "assets/quinn3.png";
+import Quinn5 from "assets/quinn4.png";
+import Quinn6 from "assets/quinn5.png";
+import Quinn7 from "assets/quinn6.png";
+import Quinn8 from "assets/quinn7.png";
 import MusicDropZone from "components/atoms/MusicDropZone";
 import ImageService from "services/image.service";
 import AuthService from "services/auth.service";
@@ -18,7 +26,23 @@ export default function SailorMoon() {
   );
   const [anim, setAnim] = useState(anime);
   const [image, setImage] = useState(true);
+  const [character, setCharacter] = useState(1);
+  const [quinn, setQuinn] = useState(0);
+  const changeQuinn = () => {
+    const new_quinn = quinn + 1;
+    if (new_quinn > 3) {
+      setQuinn(0);
+    } else {
+      setQuinn(new_quinn);
+    }
+  };
 
+  const QuinnGroup = [
+    { quinn1: Quinn1, quinn2: Quinn2 },
+    { quinn1: Quinn3, quinn2: Quinn4 },
+    { quinn1: Quinn5, quinn2: Quinn6 },
+    { quinn1: Quinn7, quinn2: Quinn8 },
+  ];
   const audioRef = useRef<any>();
   let sailorPlay = useRef<any>(null);
 
@@ -48,35 +72,96 @@ export default function SailorMoon() {
     if (user) AuthService.outfit(image, dispatch);
     else ImageService.outfit(image, dispatch);
   };
-
-  return (
-    <div className="sailor-img">
-      <h3
-        style={{ fontFamily: "'Varela Round', sans-serif", fontSize: "16px" }}
-      >
-        Sailor Moon
-      </h3>
-      {music || user?.music ? (
-        anim ? (
-          <img
-            alt="Sailor Music B"
-            src={image ? SailorB : SailorD}
-            onClick={() => outfitChange()}
-          />
+  const CharacterShowSailor = () => {
+    return (
+      <>
+        {music || user?.music ? (
+          anim ? (
+            <img
+              alt="Sailor Music B"
+              src={image ? SailorB : SailorD}
+              onClick={() => outfitChange()}
+            />
+          ) : (
+            <img
+              alt="Sailor Music A"
+              src={image ? SailorA : SailorC}
+              onClick={() => outfitChange()}
+            />
+          )
         ) : (
           <img
-            alt="Sailor Music A"
+            alt="Sailor A"
             src={image ? SailorA : SailorC}
             onClick={() => outfitChange()}
           />
-        )
-      ) : (
-        <img
-          alt="Sailor A"
-          src={image ? SailorA : SailorC}
-          onClick={() => outfitChange()}
-        />
-      )}
+        )}
+      </>
+    );
+  };
+
+  const CharacterShowQuinn = () => {
+    return (
+      <>
+        {music || user?.music ? (
+          anim ? (
+            <img
+              alt="Sailor Music B"
+              src={QuinnGroup[quinn].quinn2}
+              onClick={() => changeQuinn()}
+            />
+          ) : (
+            <img
+              alt="Sailor Music A"
+              src={QuinnGroup[quinn].quinn1}
+              onClick={() => changeQuinn()}
+            />
+          )
+        ) : (
+          <img
+            alt="Sailor A"
+            src={QuinnGroup[quinn].quinn1}
+            onClick={() => changeQuinn()}
+          />
+        )}
+      </>
+    );
+  };
+  const character1Style = character == 1 ? "white" : "black";
+  const character2Style = character == 2 ? "white" : "black";
+  return (
+    <div className="sailor-img">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "80%",
+          margin: "auto",
+        }}>
+        <h3
+          style={{
+            fontFamily: "'Varela Round', sans-serif",
+            fontSize: "16px",
+            cursor: "pointer",
+            color: `${character1Style}`,
+          }}
+          onClick={() => setCharacter(1)}>
+          Sailor Moon
+        </h3>
+        <h3
+          style={{
+            fontFamily: "'Varela Round', sans-serif",
+            fontSize: "16px",
+            cursor: "pointer",
+            color: `${character2Style}`,
+          }}
+          onClick={() => setCharacter(2)}>
+          Harley Quinn
+        </h3>
+      </div>
+      {character == 1 && <CharacterShowSailor />}
+      {character == 2 && <CharacterShowQuinn />}
       <audio
         controls
         autoPlay
@@ -89,8 +174,7 @@ export default function SailorMoon() {
         }}
         onPause={() => {
           if (sailorPlay.current) clearInterval(sailorPlay.current);
-        }}
-      >
+        }}>
         <source
           src={
             visitedUser
@@ -121,8 +205,7 @@ export default function SailorMoon() {
             color: "white",
             padding: 0,
             marginBottom: "0.83em",
-          }}
-        >
+          }}>
           Public
         </button>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -141,8 +224,7 @@ export default function SailorMoon() {
             color: visitedUser || user ? "white" : "black",
             padding: 0,
             marginBottom: "0.83em",
-          }}
-        >
+          }}>
           Private
         </button>
       </>
